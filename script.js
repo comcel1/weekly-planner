1ed13d58af1aa7b318e26d57d4abaa0da02825c5
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-01-22 10:50:07 -0600

    added date and time to js; added new time slot divs to html

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const calendar = document.querySelector("#app-calendar");
+//added Moment variable
+var rightNow = moment().format("dddd, MMMM Do, YYYY");
+// console.log(rightNow);
 
-console.log(calendar);
+// add interval counter to set current time
+var interval = setInterval(function() {
+    var momentNow = moment();
+    $('#currentDay').html(rightNow + " " + momentNow.format('hh:mm:ss A'));
+  }, 100);
+
