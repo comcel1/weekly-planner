217d81c95a069e41c0d9eeda367daea83284395c
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-01-24 16:40:44 -0600

    added save to local storage; updated HTML IDs and Classes

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 //added Moment variable
-var rightNow = moment().format("dddd, MMMM Do, YYYY");
-// console.log(rightNow);
+var currentDate = moment().format("dddd, MMMM Do, YYYY");
+// console.log(currentDate);
 
 // add interval counter to set current time
-var interval = setInterval(function() {
-    var momentNow = moment();
-    $('#currentDay').html(rightNow + " " + momentNow.format('hh:mm:ss A'));
-  }, 100);
+var interval = setInterval(function () {
+  var momentNow = moment();
+  $("#currentDay").html(currentDate + " " + momentNow.format("hh:mm:ss A"));
+}, 100);
 
+// added save to local storage
+$(document).ready(function() {
+  $(".saveBtn").on("click",function(){
+    var value = $(this).siblings(".description").val()
+    var time = $(this).parent().attr("id")
+    localStorage.setItem(time,value)
+  })
+})
