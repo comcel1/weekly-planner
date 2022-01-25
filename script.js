f305ec52f6a007dbf6457d54e1bc4c3646b18a0e
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-01-25 16:18:13 -0600

    updated hourHandler and fixed CSS to properly target and compare time of day

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// WHEN I click into a time block
-// THEN I can enter an event
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
+// // GIVEN I am using a daily planner to create a schedule
+// // WHEN I open the planner
+// // THEN the current day is displayed at the top of the calendar
+// // WHEN I scroll down
+// // THEN I am presented with time blocks for standard business hours
+// // WHEN I view the time blocks for that day
+// // THEN each time block is color-coded to indicate whether it is in the past, present, or future
+// // WHEN I click into a time block
+// // THEN I can enter an event
+// // WHEN I click the save button for that time block
+// // THEN the text for that event is saved in local storage
+// // WHEN I refresh the page
+// // THEN the saved events persist
 
 //added Moment variable
 var currentDate = moment().format("dddd, MMMM Do, YYYY");
@@ -22,41 +22,46 @@ var interval = setInterval(function () {
   $("#currentDay").html(currentDate + " " + momentNow.format("hh:mm:ss a"));
 }, 100);
 
+
 // added save to local storage
 $(document).ready(function () {
   $(".saveBtn").on("click", function () {
     var value = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
     localStorage.setItem(time, value);
-  });
+  })
+  // function to compare current time with time slot on daily planner
+  // target css classes to change color based on time of day 
+  function hourHandler() {
+    var timeNow = moment().hours()
+    $(".time-block").each(function (){
+      var hourBlock = parseInt($(this).attr("id").split("-")[1])
+        if (hourBlock < timeNow){
+            $(this).addClass("past")
+        }
+        else if (hourBlock === timeNow){
+            $(this).addClass("present")
+            $(this).removeClass("past")
+        }
+        else {
+            $(this).addClass("future")
+            $(this).removeClass("present")
 
-  // add functionality for past/present/future
-  // (target classes)
-  function updateTime() {
-    var currentTime = moment().hour();
-    $(".time-block").each(function () {
-      var timeNow = parseInt($(this).attr("id").split("-")[1]);
-      if (timeNow < currentTime) {
-        $(this).addClass("past");
-      } else if (timeNow === currentTime) {
-        $(this).addClass("present");
-        $(this).removeClass("past");
-      } else {
-        $(this).addClass("future");
-        $(this).removeClass("present");
-      }
-    });
-  }
-  updateTime();
-  $("#hour8 .description").val(localStorage.getItem("hour8"))
-  $("#hour9 .description").val(localStorage.getItem("hour9"))
-  $("#hour10 .description").val(localStorage.getItem("hour10"))
-  $("#hour11 .description").val(localStorage.getItem("hour11"))
-  $("#hour12 .description").val(localStorage.getItem("hour12"))
-  $("#hour13 .description").val(localStorage.getItem("hour13"))
-  $("#hour14 .description").val(localStorage.getItem("hour14"))
-  $("#hour15 .description").val(localStorage.getItem("hour15"))
-  $("#hour16 .description").val(localStorage.getItem("hour16"))
-  $("#hour17 .description").val(localStorage.getItem("hour17"))
-  $("#hour18 .description").val(localStorage.getItem("hour18"))
+        }
+    })
+}
+hourHandler();
+
+// pull data stored in local storage
+  $("#hour-8 .description").val(localStorage.getItem("hour-8"))
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"))
+  $("#hour-18 .description").val(localStorage.getItem("hour-18"))
 });
